45dca9b5d3a5417cddbc4fd7adeff411c1fe5ab0
Bogdan Anisimov <nonamegoose777@gmail.com>
2024-04-24 10:15:35 +0300

    Изменение отображения комплексного числа в более удобном виде

--- MainForm.cs
+++ MainForm.cs
@@ -156,7 +156,7 @@ namespace GDK_Project
 
         private string str(Complex num)
         {
-            return ("(" + num.Real.ToString() + ", " + num.Imaginary.ToString() + ")");
+            return ("(" + num.Real.ToString() + " + " + num.Imaginary.ToString() + "i)");
         }
 
         private string GetCurrentOperation()
